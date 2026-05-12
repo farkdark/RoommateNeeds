9ba165a6103bca78e5f396abd90cf05d3e8672df
farkdark <rbshinde111979@gmail.com>
2026-05-13 02:26:05 +0530

    Implement service worker caching for assets
    Added caching for assets and improved service worker installation.

--- sw.js
+++ sw.js
@@ -1,8 +1,24 @@
-// Simple Service Worker
-self.addEventListener('fetch', (event) => {
-    // Allows the app to be installable
+const CACHE_NAME = 'water-system-v1';
+const ASSETS = [
+  '/',
+  '/index.html', // Change to your actual filename if different
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
+// Fetching assets from cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
