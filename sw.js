a4f9bac92402f7fb0c29913d99d1370c8fe7fd48
farkdark <rbshinde111979@gmail.com>
2026-05-12 13:29:51 +0530

    Simplify service worker by removing unused code
    Removed unnecessary service worker event listeners and logging.

--- sw.js
+++ sw.js
@@ -1,17 +1,8 @@
-const CACHE_NAME = 'water-app-v1';
-
-// Install event - caching basic files
-self.addEventListener('install', (event) => {
-  console.log('Service Worker: Installed');
-});
-
-// Activate event
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker: Activated');
-});
-
-// Fetch event - required for PWA install criteria
+// Simple Service Worker
 self.addEventListener('fetch', (event) => {
-  // We leave this empty to let the browser handle requests normally,
-  // but the presence of the fetch listener satisfies PWA requirements.
+    // Allows the app to be installable
+});
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
 });
